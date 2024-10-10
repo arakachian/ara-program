78fcc7f2aaa47bb907fc527a5a26e2d550c39ce3
arakachian <arakachian@gmail.com>
2024-10-10 19:53:26 -0300

    Add files via upload

--- HAMBURGUESERIA_NODE/routes/admin/novedades.js
+++ HAMBURGUESERIA_NODE/routes/admin/novedades.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
+router.get('/', async function (req, res, next) {
 
-router.get('/', function (req, res, next) {
+    var novedades = await novedadesModel.getNovedades()
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        persona: req.session.nombre
+        persona: req.session.nombre,
+        novedades
     });
 });
+
+
+/* Para eliminar una novedad*/
+router.get('/eliminar/:id', async (req, res, next) => {
+    const id = req.params.id;
+
+
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades')
+}); // cierra get de eliminar 
+
+
 
 
 module.exports = router;
