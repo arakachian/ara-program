9f07beefd4d27d1cef1a7bc4a823d2912dcc183e
arakachian <arakachian@gmail.com>
2024-10-14 18:16:21 -0300

    Add files via upload

--- HAMBURGUESERIA_NODE/routes/admin/novedades.js
+++ HAMBURGUESERIA_NODE/routes/admin/novedades.js
@@ -23,6 +23,88 @@ router.get('/eliminar/:id', async (req, res, next) => {
     res.redirect('/admin/novedades')
 }); // cierra get de eliminar 
 
+/* muestra la vista */
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', { // agregar.hbs
+        layout: 'admin/layout'
+    }) // cierra render
+}) // cierra get
+
+
+/* insertar en la BD */
+router.post('/agregar', async (req, res, next) => {
+    try {
+
+        console.log(req.body) // tit, sub, cuerpo
+
+        if (req.body.titulo != "" && req.body.subtitulo != "" &&
+            req.body.cuerpo != "") {
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            })
+
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo la novedad'
+        })
+    }
+})
+
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+
+    var novedad = await novedadesModel.getNovedadById(id);
+
+    res.render('admin/modificar', { // modificar.hbs
+        layout: 'admin/layout',
+        novedad
+    });
+}) // cierro get modi
+
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        //console.log(req.body.id); // para ver si trae id
+        var obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+
+      //  console.log(obj) // para ver si trae los datos
+      
+        await novedadesModel.modificarNovedadById(obj, req.body.id);
+        res.redirect('/admin/novedades');
+    } catch (error) {
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modifico la novedad'
+        })
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
